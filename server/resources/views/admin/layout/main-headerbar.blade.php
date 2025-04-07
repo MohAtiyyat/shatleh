a903c2a3c9c908172f2abb20c940023815b90553
Hussamk01 <hussamkhaledb@gmail.com>
2025-04-08 00:15:11 +0300

    edit sidebar & code format

--- server/resources/views/admin/layout/main-headerbar.blade.php
+++ server/resources/views/admin/layout/main-headerbar.blade.php
@@ -1,5 +1,5 @@
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Right navbar links -->
+<!--<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    
     <ul class="navbar-nav ml-auto">
 
       <li class="nav-item">
@@ -12,5 +12,30 @@
           <i class="fas fa-th-large"></i>
         </a>
       </li>
+    </ul>
+</nav>-->
+
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+
+
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+                <i class="fas fa-th-large"></i>
+            </a>
+        </li>
     </ul>
 </nav>
--- server/resources/views/admin/layout/master.blade.php
+++ server/resources/views/admin/layout/master.blade.php
@@ -1,56 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  @include('admin.layout.head')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @include('admin.layout.head')
 </head>
-<body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
 
-  <!-- Navbar -->
-  @include('admin.layout.main-headerbar')
-  <!-- /.navbar -->
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
 
-  <!-- Main Sidebar Container -->
-  @include('admin.layout.main-sidebar')
+        <!-- Navbar -->
+        @include('admin.layout.main-headerbar')
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        @include('admin.layout.main-sidebar')
 
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
 
-            <h1 class="m-0"> @yield('title_div')</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#"> @yield('title_page1')</a></li>
-              <li class="breadcrumb-item active">@yield('title_page2')</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+                            <h1 class="m-0"> @yield('title_div')</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#"> @yield('title_page1')</a></li>
+                                <li class="breadcrumb-item active">@yield('title_page2')</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+
+            @yield('content')
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        @include('admin.layout.footer')
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
     </div>
+    <!-- ./wrapper -->
 
-    @yield('content')
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-
-  @include('admin.layout.footer')
-
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
-
-@include('admin.layout.footer-scripts')
+    @include('admin.layout.footer-scripts')
 </body>
+
 </html>
