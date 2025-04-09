ffdff675191795a23be5adcfd0c4a742c9017149
Mohammad Atiyyat <mohammadsami2002@gmail.com>
2025-04-09 17:20:35 +0300

    Merge remote-tracking branch 'origin/develop' into separate_route_files

--- server/routes/web.php
+++ server/routes/web.php
@@ -6,17 +6,34 @@ Route::get('/', function () {
     return view('admin.layout.master');
 });
 
-
 Route::get('/product', function () {
     return view('admin.product.all');
-});
+})->name('product');
 
-Route::get('/product/show', function () {
-    return view('admin.product.show');
-});
+Route::get('/customer', function () {
+    return view('admin.customer.all');
+})->name('customer');
+
+Route::get('/customer/create', function () {
+    return view('admin.customer.create');
+})->name('customer.create');
 
 Route::get('/product/create', function () {
     return view('admin.product.createUpdate');
-});
+})->name('product.create');
+
+
+Route::get('/order', function () {
+    return view('admin.Order.all');
+})->name('order');
+
+Route::get('/product/{id}', function () {
+    return view('admin.product.show');
+})->name('product.show');
+
+
+Route::get('/product/{id}/edit', function () {
+    return view('admin.product.createUpdate');
+})->name('product.edit');
 
 
