6e08d0fc7124bf7a8c3a6b330653a2322c0cc015
Mohammad Atiyyat <mohammadsami2002@gmail.com>
2025-04-21 15:23:46 +0300

    Implement Category management with CRUD operations and validation requests

---
+++ server/app/Http/Requests/Dashboard/Catrgory/UpdateCategoryRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Dashboard\Catrgory;
+
+use App\Traits\FormRequestTrait;
+use App\Traits\UserRuleTrait;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCategoryRequest extends FormRequest
+{
+    use FormRequestTrait, UserRuleTrait;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->admin();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'id' => $this->id('categories'),
+            'name_ar' => $this->name('categories', 'ar'),
+            'name_en' => $this->name('categories', 'en'),
+            'description_ar' => $this->description(),
+            'description_en' => $this->description(),
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'is_main' => 'nullable|boolean',
+        ];
+    }
+}
