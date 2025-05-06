8c6c6c8b97e9a49e59bd0bcd20aea73a0425ed90
Mo.Araiqat <107495343+mohammad-ariqat@users.noreply.github.com>
2025-05-06 21:34:13 +0300

    removed some of the not neededd files , and added some place holders for the main admin dashboard

--- server/routes/dashboard.php
+++ server/routes/dashboard.php
@@ -20,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::name('dashboard.')->middleware('web')->prefix('dashboard')->group(function () {
+    Route::get('/', function () {return view('admin.index');});
     // logging test route
     Route::get('/test-log', function () {
         \Log::channel('mysql')->info('Test log entry', [
